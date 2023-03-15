761e27a25f284bcfe21a4fbbf3278095a154b53a
Shawn.Liu <shawn@dynamic-005-005-036-219.5.5.pool.telefonica.de>
2023-03-15 14:08:41 +0800

    fix: last element not compared in this function

--- packages/comparison/shallow-array-equal/src/index.ts
+++ packages/comparison/shallow-array-equal/src/index.ts
@@ -2,7 +2,7 @@ export default (a: Array<any>, b: Array<any>) => {
   if (!Array.isArray(a) || !Array.isArray(b)) return false;
   if (a.length !== b.length) return false;
   const len = a.length;
-  for (let index = 0; index < len - 1; index++) {
+  for (let index = 0; index < len; index++) {
     if (a[index] !== b[index]) return false;
   }
 
