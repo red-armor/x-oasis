3245dff35d0f8ef3432a2ba891cb68d168b791fb
ryuever <youchaoliu@xiaohongshu.com>
2023-04-12 19:34:44 +0800

    feat: replace hasOwnProperty with Object.prototype.call

--- packages/predication/is-ref/src/index.ts
+++ packages/predication/is-ref/src/index.ts
@@ -1,3 +1,4 @@
 import isObject from '@x-oasis/is-object';
 
-export default (obj: any) => isObject(obj) && obj['current'];
+export default (obj: any) =>
+  isObject(obj) && Object.prototype.hasOwnProperty.call(obj, 'current');
