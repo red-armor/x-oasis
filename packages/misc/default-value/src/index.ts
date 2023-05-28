cf77ee0056bd7f4c0cfff383c8a64ef6579cd217
ryuyutyo <liuyouchao111@gmail.com>
2023-05-28 12:07:56 +0800

    feat: update

--- packages/misc/default-value/src/index.ts
+++ packages/misc/default-value/src/index.ts
@@ -1,3 +1,7 @@
+// https://stackoverflow.com/a/43157464
+// https://stackoverflow.com/a/35737590
+// https://github.com/lodash/lodash/blob/master/defaultTo.js
+
 function defaultValue(value, defaultValue) {
   /**
    * undefined == null  => true
