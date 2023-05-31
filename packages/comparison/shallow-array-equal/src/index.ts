5845cf11090167214a24711aa3a63424e20e9041
ryuever <youchaoliu@xiaohongshu.com>
2023-05-31 21:06:12 +0800

    feat: update

--- packages/comparison/shallow-array-equal/src/index.ts
+++ packages/comparison/shallow-array-equal/src/index.ts
@@ -2,6 +2,17 @@ type Comparison = (a: any, b: any) => boolean;
 
 const defaultComparison = (a, b) => a === b;
 
+const createReason = () => ({
+  source: null,
+  target: null,
+});
+
+let reason = createReason();
+
+export function getReason() {
+  return reason;
+}
+
 export default (
   a: Array<any>,
   b: Array<any>,
@@ -11,9 +22,19 @@ export default (
   if (a.length !== b.length) return false;
   const len = a.length;
   for (let index = 0; index < len; index++) {
+    const source = a[index];
+    const target = b[index];
     const falsy = comparison(a[index], b[index]);
-    if (!falsy) return false;
+    if (!falsy) {
+      if (reason) {
+        reason.source = source;
+        reason.target = target;
+      }
+      return false;
+    }
   }
+
+  reason = createReason();
 
   return true;
 };
