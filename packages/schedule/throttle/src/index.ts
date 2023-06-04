20f80591784439b82444ac3dd5c85ec52a6a8f1a
ryuyutyo <liuyouchao111@gmail.com>
2023-06-04 20:33:05 +0800

    feat: throttle

--- packages/schedule/throttle/src/index.ts
+++ packages/schedule/throttle/src/index.ts
@@ -17,45 +17,24 @@ export default function throttle(
     | boolean
 ) {
   let last = 0;
-  let timeoutHandler = null;
-  const fallback = !!fallbackOptions;
-
-  let queue = [];
+  let _args = [];
 
   return function throttled(...args: any[]) {
     const now = Date.now();
 
-    if (now - last > threshold) {
-      last = now;
-      if (timeoutHandler) {
-        clearTimeout(timeoutHandler);
-        timeoutHandler = null;
-        queue = [];
-      }
-
-      fn.apply(null, args);
-      return;
-    }
-
-    if (!fallback) return;
-
-    timeoutHandler = setTimeout(() => {
-      const len = queue.length;
-      if (len) {
-        const currentArgs = queue[len - 1];
-        queue = [];
-        fn.apply(null, currentArgs);
-      }
-    }, threshold);
-
-    let nextArgs = args;
     const persistArgs =
       typeof fallbackOptions === 'object' ? fallbackOptions.persistArgs : null;
 
+    _args = args;
+
     if (typeof persistArgs === 'function') {
-      nextArgs = persistArgs(args);
+      _args = persistArgs(args);
     }
 
-    queue.push(nextArgs);
+    if (now - last > threshold) {
+      last = now;
+
+      return fn.apply(this, args);
+    }
   };
 }
