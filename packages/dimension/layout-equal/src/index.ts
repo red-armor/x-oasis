9630ade0389e8e349a319d5c5c96a30075e05881
ryuyutyo <liuyouchao111@gmail.com>
2023-07-02 23:09:49 +0800

    feat: layout-equal add correctionValue

--- packages/dimension/layout-equal/src/index.ts
+++ packages/dimension/layout-equal/src/index.ts
@@ -5,16 +5,21 @@ const KEY_TO_CHECK = ['x', 'y', 'height', 'width'];
 export default function layoutEqual(
   oldLayout: ItemLayout,
   newLayout: ItemLayout,
-  // @ts-ignore
-  keysToCheck: Array<'x' | 'y' | 'height' | 'width'> = KEY_TO_CHECK
+  options?: {
+    // @ts-ignore
+    keysToCheck?: Array<'x' | 'y' | 'height' | 'width'> = KEY_TO_CHECK;
+    correctionValue?: number;
+  }
 ) {
   const oldLayoutType = Object.prototype.toString.call(oldLayout);
   const newLayoutType = Object.prototype.toString.call(newLayout);
+  const keysToCheck = options?.keysToCheck;
+  const correctionValue = options.correctionValue || 0;
 
   if (oldLayoutType === newLayoutType && newLayoutType === '[object Object]') {
     for (let index = 0; index < keysToCheck.length; index++) {
       const key = keysToCheck[index];
-      if (oldLayout[key] !== newLayout[key]) {
+      if (Math.abs(oldLayout[key] - newLayout[key]) > correctionValue) {
         return false;
       }
     }
