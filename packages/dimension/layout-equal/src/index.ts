c5841507e37aa1b843716190c57c9be823dda781
ryuyutyo <liuyouchao111@gmail.com>
2023-07-02 23:44:25 +0800

    fix: layout-equal issue

--- packages/dimension/layout-equal/src/index.ts
+++ packages/dimension/layout-equal/src/index.ts
@@ -6,15 +6,14 @@ export default function layoutEqual(
   oldLayout: ItemLayout,
   newLayout: ItemLayout,
   options?: {
-    // @ts-ignore
-    keysToCheck?: Array<'x' | 'y' | 'height' | 'width'> = KEY_TO_CHECK;
+    keysToCheck?: Array<'x' | 'y' | 'height' | 'width'>;
     correctionValue?: number;
   }
 ) {
   const oldLayoutType = Object.prototype.toString.call(oldLayout);
   const newLayoutType = Object.prototype.toString.call(newLayout);
-  const keysToCheck = options?.keysToCheck;
-  const correctionValue = options.correctionValue || 0;
+  const keysToCheck = options?.keysToCheck || KEY_TO_CHECK;
+  const correctionValue = options?.correctionValue || 0;
 
   if (oldLayoutType === newLayoutType && newLayoutType === '[object Object]') {
     for (let index = 0; index < keysToCheck.length; index++) {
