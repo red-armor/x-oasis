cc11c0c50a8118d129063e6bff2f2ff35eaf6096
ryuyutyo <liuyouchao111@gmail.com>
2023-10-02 23:55:09 +0800

    feat: add testing case

--- packages/struct/recycler/test/recycler.ts
+++ packages/struct/recycler/test/recycler.ts
@@ -74,6 +74,47 @@ export const basicSuite = (desc, data, fn?: any) => {
         maxIndex: 100,
       });
 
+      expect(finalizeIndices(recycler.getIndices())).toEqual({
+        mod3: ['3_mod3', '9_mod3', '21_mod3', '27_mod3', '33_mod3', '39_mod3'],
+        mod5: [
+          '0_mod5',
+          '5_mod5',
+          '10_mod5',
+          '15_mod5',
+          '20_mod5',
+          '25_mod5',
+          '30_mod5',
+          '35_mod5',
+          '40_mod5',
+          '45_mod5',
+        ],
+        mod7: ['7_mod7', '49_mod7'],
+        default: [
+          '1_default',
+          '11_default',
+          '13_default',
+          '17_default',
+          '19_default',
+          '23_default',
+          '29_default',
+          '31_default',
+          '47_default',
+          '43_default',
+        ],
+        mod2: [
+          '2_mod2',
+          '4_mod2',
+          '6_mod2',
+          '8_mod2',
+          '38_mod2',
+          '36_mod2',
+          '34_mod2',
+          '32_mod2',
+          '28_mod2',
+          '26_mod2',
+        ],
+      });
+
       console.log('get indices 2 ', finalizeIndices(recycler.getIndices()));
 
       recycler.updateIndices({
@@ -86,7 +127,55 @@ export const basicSuite = (desc, data, fn?: any) => {
         step: 1,
         maxIndex: 100,
       });
-      console.log('get indices 3 ', finalizeIndices(recycler.getIndices()));
+
+      expect(finalizeIndices(recycler.getIndices())).toEqual({
+        mod3: [
+          '3_mod3',
+          '9_mod3',
+          '21_mod3',
+          '27_mod3',
+          '33_mod3',
+          '39_mod3',
+          '51_mod3',
+        ],
+        mod5: [
+          '0_mod5',
+          '5_mod5',
+          '10_mod5',
+          '15_mod5',
+          '20_mod5',
+          '25_mod5',
+          '30_mod5',
+          '35_mod5',
+          '40_mod5',
+          '50_mod5',
+        ],
+        mod7: ['7_mod7', '49_mod7'],
+        default: [
+          '1_default',
+          '11_default',
+          '13_default',
+          '17_default',
+          '19_default',
+          '23_default',
+          '29_default',
+          '53_default',
+          '47_default',
+          '43_default',
+        ],
+        mod2: [
+          '52_mod2',
+          '54_mod2',
+          '6_mod2',
+          '8_mod2',
+          '38_mod2',
+          '36_mod2',
+          '34_mod2',
+          '32_mod2',
+          '28_mod2',
+          '26_mod2',
+        ],
+      });
 
       recycler.updateIndices({
         safeRange: {
@@ -98,7 +187,58 @@ export const basicSuite = (desc, data, fn?: any) => {
         step: 1,
         maxIndex: 100,
       });
-      console.log('get indices 4 ', finalizeIndices(recycler.getIndices()));
+
+      expect(finalizeIndices(recycler.getIndices())).toEqual({
+        mod3: [
+          '3_mod3',
+          '9_mod3',
+          '21_mod3',
+          '27_mod3',
+          '33_mod3',
+          '39_mod3',
+          '51_mod3',
+          '57_mod3',
+          '63_mod3',
+        ],
+        mod5: [
+          '55_mod5',
+          '60_mod5',
+          '10_mod5',
+          '15_mod5',
+          '20_mod5',
+          '25_mod5',
+          '30_mod5',
+          '35_mod5',
+          '40_mod5',
+          '50_mod5',
+        ],
+        mod7: ['7_mod7', '49_mod7'],
+        default: [
+          '59_default',
+          '61_default',
+          '13_default',
+          '17_default',
+          '19_default',
+          '23_default',
+          '29_default',
+          '53_default',
+          '47_default',
+          '43_default',
+        ],
+        mod2: [
+          '52_mod2',
+          '54_mod2',
+          '56_mod2',
+          '58_mod2',
+          '38_mod2',
+          '36_mod2',
+          '34_mod2',
+          '32_mod2',
+          '28_mod2',
+          '26_mod2',
+        ],
+      });
+
       fn.data.delete(20);
 
       recycler.updateIndices({
@@ -111,7 +251,56 @@ export const basicSuite = (desc, data, fn?: any) => {
         step: 1,
         maxIndex: 100,
       });
-      console.log('get indices 5 ', finalizeIndices(recycler.getIndices()));
+      expect(finalizeIndices(recycler.getIndices())).toEqual({
+        mod3: [
+          '3_mod3',
+          '9_mod3',
+          '21_mod3',
+          '27_mod3',
+          '33_mod3',
+          '39_mod3',
+          '51_mod3',
+          '57_mod3',
+          '63_mod3',
+        ],
+        mod5: [
+          '55_mod5',
+          '60_mod5',
+          '65_mod5',
+          '15_mod5',
+          undefined,
+          '25_mod5',
+          '30_mod5',
+          '35_mod5',
+          '45_mod5',
+          '50_mod5',
+        ],
+        mod7: ['7_mod7', '49_mod7'],
+        default: [
+          '59_default',
+          '61_default',
+          '31_default',
+          '37_default',
+          '19_default',
+          '23_default',
+          '41_default',
+          '53_default',
+          '47_default',
+          '43_default',
+        ],
+        mod2: [
+          '52_mod2',
+          '54_mod2',
+          '56_mod2',
+          '58_mod2',
+          '38_mod2',
+          '36_mod2',
+          '34_mod2',
+          '32_mod2',
+          '28_mod2',
+          '26_mod2',
+        ],
+      });
     });
   });
 };
