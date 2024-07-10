685310d5885b62c79ebfa2a1027e906310a4d8e7
ryuyutyo <liuyouchao111@gmail.com>
2024-07-11 00:35:19 +0800

    feat: update

--- packages/assertion/is/src/index.ts
+++ packages/assertion/is/src/index.ts
@@ -8,7 +8,7 @@ export default (x: any, y: any) => {
     // Steps 1-5, 7-10
     // Steps 6.b-6.e: +0 != -0
     // Added the nonzero y check to make Flow happy, but it is redundant
-    return y !== 0 || x !== 0 || 1 / x === 1 / y;
+    return x !== 0 || y !== 0 || 1 / x === 1 / y;
   }
   // Step 6.a: NaN == NaN
   return x !== x && y !== y;
