f68b5f734144c2d979d38831a0421476215840ad
ryuyutyo <liuyouchao111@gmail.com>
2024-09-28 19:19:32 +0800

    feat: x

--- packages/struct/integer-buffer-set/src/index.ts
+++ packages/struct/integer-buffer-set/src/index.ts
@@ -1,5 +1,5 @@
-import invariant from '@x-oasis/invariant';
 import isClamped from '@x-oasis/is-clamped';
+import invariant from '@x-oasis/invariant';
 import returnHook, { ReturnHook } from '@x-oasis/return-hook';
 import Heap from '@x-oasis/heap';
 import {
